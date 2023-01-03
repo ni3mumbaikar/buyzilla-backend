2d2ae19567cbde369bacbd03038c39433ffb8c03
ni3mumbaikar <nitinkale035@gmail.com>
2023-01-03 11:38:21 +0530

    Add : Delete shipper endpoint

--- src/main/java/com/buyzilla/dev/code/controller/ShippersController.java
+++ src/main/java/com/buyzilla/dev/code/controller/ShippersController.java
@@ -3,22 +3,30 @@ package com.buyzilla.dev.code.controller;
 import com.buyzilla.dev.code.entity.Shipper;
 import com.buyzilla.dev.code.service.ShipperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletResponse;
+import java.net.http.HttpResponse;
 import java.util.List;
 
 @RestController
 @CrossOrigin
+@RequestMapping("/api/v1/shippers")
 public class ShippersController {
 
     @Autowired
     ShipperService shipperService;
 
-    @GetMapping("/api/v1/shippers")
+    @GetMapping
     ResponseEntity<List<Shipper>> getShippers(){
         return shipperService.getShippers();
     }
+
+    @DeleteMapping("/{sid}")
+    ResponseEntity<String> deleteShipper(@PathVariable Integer sid){
+        return ResponseEntity.ok(shipperService.deleteShipper(sid));
+    }
+
 }
--- src/main/java/com/buyzilla/dev/code/service/ShipperService.java
+++ src/main/java/com/buyzilla/dev/code/service/ShipperService.java
@@ -1,9 +1,11 @@
 package com.buyzilla.dev.code.service;
 
 import com.buyzilla.dev.code.entity.Shipper;
+import com.buyzilla.dev.code.exceptions.ShipperNotFoundException;
 import com.buyzilla.dev.code.respository.ShipperRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
@@ -16,7 +18,17 @@ public class ShipperService {
 
     @Autowired
     ShipperRepository shipperRepository;
+
+    @Autowired
+    Environment environment;
+
     public ResponseEntity<List<Shipper>> getShippers() {
         return new ResponseEntity<>(shipperRepository.findAll(), HttpStatus.OK);
+    }
+
+    public String deleteShipper(Integer sid) {
+        System.out.println(shipperRepository.findById(sid));
+        shipperRepository.delete(shipperRepository.findById(sid).orElseThrow(() -> new ShipperNotFoundException(environment.getProperty("shipper_not_found"))));
+        return "Shipper deleted";
     }
 }
